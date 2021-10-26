74484d69cf6a89bae24e2dda688a590704abd3ca
supermancash <alexanderio.emilio@gmail.com>
2021-10-26 15:13:43 +0200

    adding push.config

---
+++ backend/config/push.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicKey: 'BLu-niPp_sPIzQyEYCyv-nncF824MjY0UPMaGXwm-8PH1FnAiOoQePUshBFogdIA7YYaxnjg8bd2JG8iTJwTSwI',
+    privateKey: 'Be7J7wbnb7i6GEgBsRO2OCKQK6_RvEmJdaD-_axQxGw'
+}
