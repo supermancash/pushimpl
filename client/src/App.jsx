9df58941d9e2cb8c50373c02f53efddb3040b932
etriest <emil.triest@telekom.de>
2021-10-27 11:35:49 +0200

    fixing sw bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useEffect} from "react";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
 
 import LoginMask from "./components/LoginMask";
 import Subscribe from "./components/Subscribe";
 
 const App = () => {
-    useEffect(() => {
-        const registersw = async () => {
-            const sw = await navigator.serviceWorker.register('./sw.js');
-            console.log(sw);
-        }
-        registersw().catch(err => console.log(err));
-    })
-
     return (
         <Router>
             <Route exact path="/" component={Subscribe}/>
-            <Route path="/admin" component={LoginMask} />
+            <Route path="/admin" component={LoginMask}/>
         </Router>
-);
+    );
 }
 
 export default App;
