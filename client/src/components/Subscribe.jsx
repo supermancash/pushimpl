d930965eb1cc801f60717ff873e88eaa5982ee98
supermancash <alexanderio.emilio@gmail.com>
2021-11-09 17:28:48 +0100

    finally fixed sw bug

--- client/src/components/Subscribe.jsx
+++ client/src/components/Subscribe.jsx
@@ -1,60 +1,62 @@
 import Button from "react-bootstrap/Button";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import path from 'path';
 
 const Subscribe = () => {
     const [permission, setPermission] = useState(false);
     const [sw, setSw] = useState({});
 
+    useEffect(() => {
+        const checkPermission = async () => {
+            await setSw(await navigator.serviceWorker.ready);
+            let subscription = null;
+            console.log(sw)
+            if (sw !== {}) subscription = await sw.pushManager.getSubscription();
+            subscription === null ? setPermission(false) : setPermission(true);
+        }
+        registersw().then(() => {
+            checkPermission().catch(err => console.log(err))
+        })
+    });
+
     const registersw = async () => {
-        console.log(path.resolve('sw.js'))
-        await setSw(await navigator.serviceWorker.register(path.resolve('sw.js')));
-        if (sw === {}) registersw().catch(err => console.log(err));
-        console.log(sw);
+        setSw(await navigator.serviceWorker.register(path.resolve('sw.js')));
     }
 
-    const checkPermission = async () => {
-        await setSw(await navigator.serviceWorker.ready);
-        const subscription = await sw.pushManager.getSubscription();
-        subscription === null ? checkPermission().catch(err => console.log(err)) : setPermission(true);
-    }
 
     const subscribeHandler = async () => {
-        await registersw().then(async () => {
-            await checkPermission().then(async () => {
-                if ('PushManager' in window) {
-                    await setSw(await navigator.serviceWorker.ready);
-                    const push = await sw.pushManager.subscribe({
-                        userVisibleOnly: true,
-                        applicationServerKey: 'BLu-niPp_sPIzQyEYCyv-nncF824MjY0UPMaGXwm-8PH1FnAiOoQePUshBFogdIA7YYaxnjg8bd2JG8iTJwTSwI'
-                    });
+        if ('PushManager' in window) {
+            await setSw(await navigator.serviceWorker.ready);
+            const push = await sw.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: 'BLu-niPp_sPIzQyEYCyv-nncF824MjY0UPMaGXwm-8PH1FnAiOoQePUshBFogdIA7YYaxnjg8bd2JG8iTJwTSwI'
+            });
 
-                    if (!permission) {
-                        await fetch("/api/subscribers", {
-                            method: 'POST',
-                            credentials: 'omit',
-                            mode: 'cors',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Access-Control-Request-Headers': '*'
-                            },
-                            body: JSON.stringify(push)
-                        }).catch((error) => {
-                            console.error('Error:', error);
-                        });
-                    }
-                    setPermission(true)
-                }
-            })
-        });
+            if (!permission) {
+                await fetch("/api/subscribers", {
+                    method: 'POST',
+                    credentials: 'omit',
+                    mode: 'cors',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Request-Headers': '*'
+                    },
+                    body: JSON.stringify(push)
+                }).catch((error) => {
+                    console.error('Error:', error);
+                });
+            }
+            setPermission(true);
+        }
     }
 
-    return (
-        <div>
-            {permission ? <p>You are subscribed!</p> :
-                <Button variant="primary" onClick={subscribeHandler}>Subscribe</Button>}
-        </div>
-    );
+
+return (
+    <div>
+        {permission ? <p>You are subscribed!</p> :
+            <Button variant="primary" onClick={subscribeHandler}>Subscribe</Button>}
+    </div>
+);
 }
 
 export default Subscribe;
